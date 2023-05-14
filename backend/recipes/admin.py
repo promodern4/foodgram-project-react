2414002cd438c499ae645ddec8de9fe5c3808c4c
Павел Полетаев <pasha.poletaev14@mail.ru>
2023-05-14 21:00:28 +0300

    fix isort

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from .models import (Cart,
-                     Favourite,
-                     Ingredient,
-                     Recipe,
-                     RecipeIngredient,
-                     Tag)
+from .models import Cart, Favourite, Ingredient, Recipe, RecipeIngredient, Tag
 
 
 class ItemInline(admin.StackedInline):
